a72224af2dc642488550cbf7c047d3344a3d7174
MonicaJonikaites <monicacjonikaites@gmail.com>
2026-08-21 12:12:00 -0300

    Tarefa 1 concluida e Tarefa 2

---
+++ TAREFA01/script.js
@@ -0,0 +1,56 @@
+const tela = document.getElementById("tela");
+const ctx = tela.getContext("2d");
+
+const cor = document.getElementById("cor");
+const limpar = document.getElementById("limpar");
+const exportar = document.getElementById("exportar");
+
+let desenhando = false;
+
+ctx.lineWidth = 5;
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
+
+tela.addEventListener("mousedown", function (e) {
+    desenhando = true;
+
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+})
+
+tela.addEventListener("mousemove", function (e) {
+    if (!desenhando) return;
+
+    ctx.strokeStyle = cor.value;
+
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+});
+
+tela.addEventListener("mouseup", function () {
+    desenhando = false;
+    ctx.beginPath();
+});
+
+tela.addEventListener("mouseleave", function () {
+    desenhando = false;
+    ctx.beginPath();
+});
+
+
+limpar.addEventListener("click", function () {
+    ctx.clearRect(0, 0, tela.width, tela.height);
+    location.reload();
+
+});
+
+exportar.addEventListener("click", function () {
+    const link = document.createElement("a");
+
+    link.download = "minipaint.png";
+    link.href = tela.toDataURL("image/png");
+
+    link.click();
+});
+const status = document.getElementById("status");
+
